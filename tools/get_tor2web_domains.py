61a03809eaac7946981d889a24dee2acbf89626f
Juha Nurmi <juha.nurmi@ahmia.fi>
2014-05-06 20:40:12 +0300

    Integration with Tor2web: gather data from tor2web nodes. In future, ahmia will add every new domain to its database and use the statistics (visit count) as a popularity measure. This script tools/get_tor2web_domains.py is the start.

---
+++ tools/get_tor2web_domains.py
@@ -0,0 +1,60 @@
+"""Load each tor2web node's visited domain list."""
+# -*- coding: utf-8 -*-
+import urllib3 #To HTTP requests
+import codecs # UTF-8 support for the text files
+import json # JSON library
+import datetime # Timestamp to each JSON file
+
+def text2file(txt, filename):
+    """Write the txt to the file."""
+    outputfile = codecs.open(filename, "w", "utf-8")
+    outputfile.write(txt)
+    outputfile.close()
+
+def valid_pretty_json(myjson):
+    """Check that text string is valid JSON."""
+    try:
+        parsed = json.loads(myjson)
+        pretty = json.dumps(parsed, indent=4, sort_keys=True,
+        ensure_ascii=False)
+        return pretty
+    except ValueError:
+        return False
+
+def loader(tor2web_nodes):
+    """Load visited domains information from tor2web nodes."""
+    for node in tor2web_nodes:
+        print "\n Trying download from the %s \n" % node
+        json_data = get_json("abcd." + node)
+        if not json_data:
+            continue
+        pretty_json = valid_pretty_json(json_data)
+        if pretty_json:
+            timestamp = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+            node = node.replace(".", "") + "_"
+            filename = "../tor2web_stats/" + node + timestamp + ".json"
+            text2file(pretty_json, filename)
+            print "Downloaded JSON data."
+
+def get_json(node):
+    """Send HTTP GET request to download JSON list."""
+    try:
+        http = urllib3.HTTPSConnectionPool(node, 443, timeout=10,
+        cert_reqs='CERT_NONE', assert_hostname=False)
+        response = http.request('GET', "/antanistaticmap/stats/yesterday")
+    except Exception as error:
+        print error
+        return ""
+    if response.status == 200:
+        return response.data
+    else:
+        return ""
+
+def main():
+    """Main function."""
+    tor2web_nodes = ["tor2web.fi", "tor2web.org",
+    "onion.to", "tor2web.blutmagie.de"]
+    loader(tor2web_nodes)
+
+if __name__ == '__main__':
+    main()

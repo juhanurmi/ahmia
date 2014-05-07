3ccd89aa5d66e7ca0324376726c0187d6151de2f
Juha Nurmi <juha.nurmi@ahmia.fi>
2014-05-07 07:24:59 +0300

    Updated Solr tools.

--- tools/new_domais_from_solr.py
+++ tools/new_domais_from_solr.py
@@ -1,87 +1,89 @@
+"""Read hidden service domains from YaCy's Solr."""
 # -*- coding: utf-8 -*-
-
 from urllib2 import Request
 import urllib2
 import base64
 import xml.etree.ElementTree as ET #XML
 import re
 
-#test is url correct onion url
-#Must be like http://3g2upl4pq6kufc4m.onion/
-def validate_onion_URL( url ):
+def validate_onion_url(url):
+    """Test that the URL a valid onion URL."""
+    # Must be like http://3g2upl4pq6kufc4m.onion/
     if len(url) != 30:
         return False
     if url[0:7] != 'http://':
         return False
     if url[-7:] != '.onion/':
         return False
-    if not re.match( "[a-z2-7]{16}", url[7:-7] ):
+    if not re.match("[a-z2-7]{16}", url[7:-7]):
         return False
     return True
 
+def open_connection(req):
+    """Request handler."""
+    try:
+        handle = urllib2.urlopen(req)
+        if handle.getcode() != 200:
+            print handle.getcode()
+        else:
+            print handle.read()
+            return True
+    except urllib2.HTTPError, error:
+        print 'HTTPError = ' + str(error.code)
+    except urllib2.URLError, error:
+        print 'URLError = ' + str(error.reason)
+    except Exception:
+        import traceback
+        print 'generic exception: ' + traceback.format_exc()
+    return False
 
-def open(req):
-	try:
-		handle = urllib2.urlopen(req)
-		if handle.getcode() != 200:
-			print handle.getcode()
-		else:
-			return handle.read()
-	except urllib2.HTTPError, e:
-		print('HTTPError = ' + str(e.code))
-	except urllib2.URLError, e:
-		print('URLError = ' + str(e.reason))
-	except Exception:
-		import traceback
-		print('generic exception: ' + traceback.format_exc())
-	return False
-
-#BASE64_CREDENTIALS=$(echo -n "<username>:<password>" | base64)
-#curl -i --header "Authorization:Basic ${BASE64_CREDENTIALS}" <url>
 def auth(req):
-        username = 'solr-access'
-        password = 'E51Zs9l9Gfd7kzN67v9OldTEVw'
-        base64string = base64.encodestring('%s:%s' % (username, password))[:-1]
-        header = "Basic %s" % base64string
-        req.add_header("Authorization", header)
-
+    """Basic authentication."""
+    #BASE64_CREDENTIALS=$(echo -n "<username>:<password>" | base64)
+    #curl -i --header "Authorization:Basic ${BASE64_CREDENTIALS}" <url>
+    username = 'solr-access'
+    password = 'E51Zs9l9Gfd7kzN67v9OldTEVw'
+    base64string = base64.encodestring('%s:%s' % (username, password))[:-1]
+    header = "Basic %s" % base64string
+    req.add_header("Authorization", header)
 
 def send_get(url):
-        req = Request(url)
-        auth(req)
-	result = open(req)
-        if not result:
-		print "Error..."
-		return ""
-
-	return result
-
+    """Authenticate and send GET request to the URL."""
+    req = Request(url)
+    auth(req)
+    result = open(req)
+    if not result:
+        print "Error..."
+        return ""
+    return result
 
 def add_hidden_services(start):
-	found = False
-	start_str = '%d' % start
-	#urldomains = 'http://10.8.0.6/solr/select?q=host_s:*&defType=edismax&start=0&core=collection1&fl=host_s&facet=on&facet.field=host_s&rows=9999'
-	urldomains = 'http://10.8.0.6/solr/select?q=host_s:*&defType=edismax&start='+start_str+'&core=collection1&fl=host_s&rows=10100'
-	XML_string = send_get( urldomains )
-	try:
-		root = ET.fromstring(XML_string) #<response>
-		result = root.find('result') #<result>
-		for doc in result.findall('doc'):
-			for str in doc.findall('str'):
-				if "host_s" in str.get('name'):
-					domain = "http://"+str.text+"/"
-					if validate_onion_URL(domain):		
-						print('{"url":"'+domain+'"}')		
-						found = True
-	except:
-		print XML_string		
+    """Print a list of hidden services from Solr."""
+    found = False
+    start_str = '%d' % start
+    urldomains = 'http://10.8.0.10:8080/solr/select?q=host_s:*&defType=edismax&start='+start_str+'&core=collection1&fl=host_s&rows=10100'
+    xml_string = send_get(urldomains)
+    try:
+        root = ET.fromstring(xml_string) #<response>
+        result = root.find('result') #<result>
+        for doc in result.findall('doc'):
+            for string in doc.findall('str'):
+                if "host_s" in string.get('name'):
+                    domain = "http://"+string.text+"/"
+                    if validate_onion_url(domain):
+                        print '{"url":"'+domain+'"}'
+                        found = True
+    except:
+        print xml_string
+    return found
 
-	return found
-
+def main():
+    """Main function."""
+    start = 0
+    while True:
+        if not add_hidden_services(start):
+            break
+        start = start + 10000
 
 if __name__ == '__main__':
-	start = 0
-	while True:
-		if not add_hidden_services(start):
-			break
-		start = start + 10000
+    main()

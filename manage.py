6a868f098fe3a0dc7b55ce06232054f200955fe6
Juha Nurmi <juha.nurmi@ahmia.fi>
2014-06-04 13:34:37 +0300

    Django version updated from 1.4 to 1.6: resolving relative path problems with Apache WSGI.

--- manage.py
+++ manage.py
@@ -5,6 +5,6 @@ import sys
 
 if __name__ == "__main__":
     # Path to the settings.py file
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ahmia.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)

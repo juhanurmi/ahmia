fe1d1b7822c2722218105df7ddf7fa49c7dd6180
Brian Sloan <bsloan914@gmail.com>
2015-01-11 17:14:19 -0500

    oops. getting rid of silly debug print statement

--- tools/middlewares.py
+++ tools/middlewares.py
@@ -3,5 +3,4 @@ from scrapy.conf import settings
 class ProxyMiddleware(object):
     def process_request(self, request, spider):
         if spider.name == "test_services_spider":
-            print "THE SPIDER IS TEST SERVICES SPIDER!"
             request.meta["proxy"] = settings.get("HTTP_PROXY")
